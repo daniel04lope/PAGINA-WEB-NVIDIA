90594f9beac9123b814315b7bf4b097fee39a955
daniel04lope <daniel04lope@gmail.com>
2025-01-23 14:20:52 +0100

    Footer casi finalizado

--- src/app/main-page/Pages/home/home.component.ts
+++ src/app/main-page/Pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../Components/header/header.component';
 import { CarouselComponent } from '../../Components/carousel/carousel.component';
+import { FooterComponent } from '../../Components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CarouselComponent],
+  imports: [HeaderComponent, CarouselComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

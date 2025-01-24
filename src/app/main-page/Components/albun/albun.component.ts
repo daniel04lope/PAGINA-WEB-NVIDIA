c62cf464563fd3a934f9cefae40e024f0c3e4ef1
daniel04lope <daniel04lope@gmail.com>
2025-01-24 10:33:09 +0100

    Albun creado

---
+++ src/app/main-page/Components/albun/albun.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-albun',
+  standalone: true,
+  imports: [],
+  templateUrl: './albun.component.html',
+  styleUrl: './albun.component.css'
+})
+export class AlbunComponent {
+
+}
--- src/app/main-page/Pages/home/home.component.ts
+++ src/app/main-page/Pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../Components/header/header.component';
 import { CarouselComponent } from '../../Components/carousel/carousel.component';
 import { FooterComponent } from '../../Components/footer/footer.component';
+import { AlbunComponent } from '../../Components/albun/albun.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CarouselComponent,FooterComponent],
+  imports: [HeaderComponent, CarouselComponent,FooterComponent,AlbunComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
